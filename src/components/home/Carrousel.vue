d3bf40144b480f06b98798c2dd76b3fd42be5cad
rulerule <andre.s.goncalves@tecnico.ulisboa.pt>
2018-11-17 19:08:28 +0000

    carousel

--- src/components/home/Carrousel.vue
+++ src/components/home/Carrousel.vue
@@ -1,14 +1,61 @@
 <template>
   <div class="row carrousel-wrapper">
-    <h1>Carrousel</h1>
+    <b-carousel id="carousel1"
+                controls
+                indicators
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide>
+        <img slot="img" src="../../assets/image-1.jpg"/>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <img slot="img" src="../../assets/image-1.jpg"/>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <img slot="img" src="../../assets/image-1.jpg"/>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <img slot="img" src="../../assets/image-1.jpg"/>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <img slot="img" src="../../assets/image-1.jpg"/>
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss">
 .carrousel-wrapper {
-  height:400px;
+  height:600px;
+  #carousel1 {
+    margin:0 auto;
+    width:100%;
+    height:600px;
+    overflow:hidden;
+  }
 }
 </style>
